6538e60e74b8b7e11e75828161de738f4102668e
Trixia Mae Banzon <153486933+trixiabanzon@users.noreply.github.com>
2026-07-25 21:51:16 +0800

    feat: deleted Interests

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-import Interests from "./components/Interests";
 
 // 1. INTERACTIVE PARTICLE NETWORK
 function ParticleNetwork() {
@@ -150,7 +149,6 @@ export default function App() {
             <About />
             <Experience />
             <Projects />
-            <Interests />
           </main>
         </div>
       </div>
